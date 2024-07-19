4678d3f4b836485e6487605b025b00aa3ca19620
Victor Fernández Quintana <vifernandez@baufest.com>
2024-07-19 12:22:14 -0500

    LambdaExample1 with Functional Interface Runnable

--- src/main/java/com/functional/lambda/LambdaExample1.java
+++ src/main/java/com/functional/lambda/LambdaExample1.java
@@ -2,12 +2,8 @@ package com.functional.lambda;
 
 public class LambdaExample1 {
     public static void main(String[] args) {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Thread Executed!");
-            }
-        });
+        Runnable runnable = () -> System.out.println("Thread Executed!");
+        Thread thread = new Thread(runnable);
         thread.start();
     }
 }
