471bc12aaf18c51596f8f5cb3b59f4a9f729734d
Victor Fernández Quintana <vifernandez@baufest.com>
2024-07-19 12:54:49 -0500

    Add my interface functional on the fly

--- src/main/java/com/functional/lambda/FunctionalInterfaceDemo.java
+++ src/main/java/com/functional/lambda/FunctionalInterfaceDemo.java
@@ -2,7 +2,9 @@ package com.functional.lambda;
 
 public class FunctionalInterfaceDemo {
     public static void main(String[] args) {
-        MyFunInterface myFunInterface = () -> System.out.println("Hello!");
-        myFunInterface.myMethod();
+        onTheFly(()-> System.out.println("Hello!"));
+    }
+    public static void onTheFly(MyFunInterface fun){
+        fun.myMethod();
     }
 }
