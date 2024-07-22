35c0626ac8fdaea23945e48111262e37618b48ed
Victor Fernández Quintana <vifernandez@baufest.com>
2024-07-21 20:57:40 -0500

    Add example for Method with multiple statements

--- src/main/java/com/functional/lambda/LambdaPractice3.java
+++ src/main/java/com/functional/lambda/LambdaPractice3.java
@@ -2,7 +2,11 @@ package com.functional.lambda;
 
 public class LambdaPractice3 {
     public static void main(String[] args) {
-        LengthOfString len = s -> s.length();
-        System.out.println(len.length("Victor"));
+        LengthOfString len = s -> {
+            int l = s.length();
+            System.out.println("Length of the String is " + l);
+            return l;
+        };
+        len.length("Victor");
     }
 }
